43e1a5b51b0ed7b5569354fc801c84b6f662cbd1
qingjie <qzhao02@syr.edu>
2017-12-09 09:21:51 -0500

    Add files via upload

---
+++ feign+ribbon/homecare-v1-core/src/main/java/com/homecare/core/api/HiController.java
@@ -0,0 +1,23 @@
+package com.homecare.core.api;
+
+import com.homecare.core.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhao on 12/6/17.
+ */
+
+@RestController
+public class HiController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ feign+ribbon/homecare-v1-discovery/src/test/java/com/homecare/HomecareDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homecare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomecareDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
